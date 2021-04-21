853968d3da02c06030d088ca630f3464cefb8819
Oliver Saxon <oliverrsaxon@gmail.com>
2021-04-21 16:49:06 +0100

    FEAT: Readme now creates markdown formatted file with basic structure in place

---
+++ README.md
@@ -0,0 +1,17 @@
+# Good ReadMe Generator
+
+  
+## Description
+This application will create a readme in markdown format following best practice guidelines.
+
+  
+## Installation Instructions
+Run from the terminal
+
+  
+## Project Contributors
+Oliver Saxon
+
+  
+## Licence
+B/n
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,24 +4,29 @@ function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  let title = `# ${data.title}`;
-  let desc = `## Description\n${data.desc}`;
-  const fileData = [];
+  const title = `# ${data.title}`;
+  const desc = `## Description\n${data.desc}`;
+  const install = `## Installation Instructions\n${data.install}`;
+  const contributors = `## Project Contributors\n${data.contributors}`;
+  const licence = `## Licence\n${data.licence}`;
 
-  fileData.push(title);
-  fileData.push(desc);
-
-  const fileDataString = fileData.join("\n");
-  return `# ${data.title}
-  ${desc}\n
+  return `# ${data.title}\n
+  \n${desc}\n
+  \n${install}\n
+  \n${contributors}\n
+  \n${licence}/n
 `;
 }
 
