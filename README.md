013a5733e2b2e76e063adc1162df825c0b121cc9
Oliver Saxon <oliverrsaxon@gmail.com>
2021-04-22 15:21:48 +0100

    FEAT: Licence section function

--- README.md
+++ README.md
@@ -1,18 +1,21 @@
-# title
+# 
 
   
 ## Description
-desc
+
 
   
 ## Installation Instructions
-install
+
 
   
-## Project Contributors
-cont
+## Credits
+
 
   
 ## Licence
 [![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)
+
+All assets and code are under the [Creative Commons](http://creativecommons.org/publicdomain/zero/1.0/) and in the public domain unless specified otherwise.
+
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,45 +1,104 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  license = license.replace(/\s/g, '');
+  license = license.replace(/\s/g, "");
   license = license.toLowerCase();
   let badge;
-  /*const badges = {
-    creativecommons: "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)",
-    gnugplv3: "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
-    ibm: "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)",
-    isc: "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
-    mit: "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
-  }*/
 
-  switch(license){
-    case 'creativecommons':
-      badge = "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)"
+  switch (license) {
+    case "creativecommons":
+      badge =
+        "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)";
+      break;
+    case "gnugplv3":
+      badge =
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "ibm":
+      badge =
+        "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+      break;
+    case "isc":
+      badge =
+        "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+      break;
+    case "mit":
+      badge =
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
       break;
   }
-  return badge
+  return badge;
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
 
+  switch (license) {
+    case "creativecommons":
+      link =
+        "[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)";
+      break;
+    case "gnugplv3":
+      link =
+        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "ibm":
+      link =
+        "[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)";
+      break;
+    case "isc":
+      link =
+        "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+      break;
+    case "mit":
+      link =
+        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+  }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+  let link;
+  let licenseTrimmed = license.replace(/\s/g, "");
+  licenseTrimmed = licenseTrimmed.toLowerCase();
 
+  switch (licenseTrimmed) {
+    case "creativecommons":
+      link =
+        "(http://creativecommons.org/publicdomain/zero/1.0/)";
+      break;
+    case "gnugplv3":
+      link =
+        "(https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "ibm":
+      link =
+        "(https://opensource.org/licenses/IPL-1.0)";
+      break;
+    case "isc":
+      link =
+        "(https://opensource.org/licenses/ISC)";
+      break;
+    case "mit":
+      link =
+        "(https://opensource.org/licenses/MIT)";
+      break;
+  }
+
+  return `\nAll assets and code are under the [${license}]${link} and in the public domain unless specified otherwise.\n`
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  console.log(data)
+  console.log(data);
   const title = `# ${data.title}`;
   const desc = `## Description\n${data.desc}`;
   const install = `## Installation Instructions\n${data.install}`;
-  const contributors = `## Project Contributors\n${data.contributors}`;
-  const licence = `## Licence\n${renderLicenseBadge(data.license)}`;
+  const contributors = `## Credits\n${data.contributors}`;
+  const licence = `## Licence\n${renderLicenseBadge(data.license)}\n${renderLicenseSection(data.license)}`;
 
   return `# ${data.title}\n
   \n${desc}\n
