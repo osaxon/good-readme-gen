85f4872776aaabb712f1dc27396f16bb4d54fc8f
Oliver Saxon <oliverrsaxon@gmail.com>
2021-04-24 12:23:55 +0100

    CONTENT: Removed comments and console logs

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 
-// TODO: Create an array of questions for user input
+
 const questions = [{
     type: 'input',
     message: 'Enter the title of your project',
@@ -51,7 +50,7 @@ const questions = [{
 
 
 
-// TODO: Create a function to write README file
+
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) =>{
         if(err){
@@ -66,7 +65,7 @@ function createFile(data) {
     writeToFile("README.md", generateMarkdown(data));
 }
 
-// TODO: Create a function to initialize app
+
 function init() {
     inquirer
     .prompt(questions)
@@ -79,5 +78,5 @@ function init() {
     
 }
 
-// Function call to initialize app
+
 init();
